20f990a876894dcf03927ac94fefdb33dc206383
huangchangweng <599139419@qq.com>
2016-04-22 15:12:45 +0800

    init commit

---
+++ Example/Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by HCW on 16/4/22.
+//  Copyright © 2016年 HCW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Example/Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Example
+//
+//  Created by HCW on 16/4/22.
+//  Copyright © 2016年 HCW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
